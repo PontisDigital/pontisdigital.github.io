8f4dcbe8c71b8276d6319ffbc21ec244ea0ecbb7
Sam Collier <sam@samcollier.tech>
2022-09-06 01:54:54 +0100

    Supertokens

--- src/App.js
+++ src/App.js
@@ -3,14 +3,51 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages'
 import LoginPage from './pages/login';
 
+import SuperTokens, { SuperTokensWrapper, getSuperTokensRoutesForReactRouterDom} from "supertokens-auth-react";
+import ThirdPartyEmailPassword, {Github, Google, Facebook, Apple} from "supertokens-auth-react/recipe/thirdpartyemailpassword";
+import Session from "supertokens-auth-react/recipe/session";
+import * as reactRouterDom from "react-router-dom";
+import axios from "axios";
 function App() {
+SuperTokens.init({
+    appInfo: {
+        // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
+        appName: "Rainyday",
+        apiDomain: "https://api.pontis.digital",
+        websiteDomain: "https://rainyday.deals",
+        apiBasePath: "/auth",
+        websiteBasePath: "/login"
+    },
+    recipeList: [
+        ThirdPartyEmailPassword.init({
+            signInAndUpFeature: {
+                providers: [
+                    Github.init(),
+                    Google.init(),
+                    Facebook.init(),
+                    Apple.init(),
+                ]
+            }
+        }),
+        Session.init()
+    ]
+});
+
+	let axiosInstance = axios.create({
+		baseURL: 'https://api.pontis.digital/',
+		timeout: 1000,
+		headers: {'Content-Type': 'application/json'}
+	})
+
   return (
-	  <Router>
-		  <Routes>
-			  <Route path="/" element={<Home/>}/>
-			  <Route path="/login" element={<LoginPage/>}/>
-		  </Routes>
-	  </Router>
+	  <SuperTokensWrapper>
+		  <Router>
+			  <Routes>
+				  <Route path="/" element={<Home/>}/>
+				  {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
+			  </Routes>
+		  </Router>
+	  </SuperTokensWrapper>
   );
 }
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,22 @@
+import SuperTokens, { SuperTokensWrapper, getSuperTokensRoutesForReactRouterDom} from "supertokens-auth-react";
+import ThirdPartyEmailPassword, {Github, Google, Facebook, Apple} from "supertokens-auth-react/recipe/thirdpartyemailpassword";
+import Session from "supertokens-auth-react/recipe/session";
+import {
+	BrowserRouter,
+	Routes,
+} from "react-router-dom";
+import * as reactRouterDom from "react-router-dom";
+
 const LoginPage = () => {
+
 	return(
-		<>
-		</>
+		<SuperTokensWrapper>
+			<BrowserRouter>
+				<Routes>
+					{getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
+				</Routes>
+			</BrowserRouter>
+		</SuperTokensWrapper>
 	)
 }
 
