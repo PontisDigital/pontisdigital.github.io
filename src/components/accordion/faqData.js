fe3c36ba00bca9f7a40e2eefecbc5ff9e85fbf7f
Sam Collier <sam@samcollier.tech>
2022-11-07 23:46:59 +0000

    stay down footer

--- src/components/accordion/faqData.js
+++ src/components/accordion/faqData.js
@@ -4,12 +4,4 @@ export const faqData = [
 		question: 'Where is the money coming from?',
 		answer: 'Rainyday enters into agreements with merchants. We get a commission on all purchases in exchange for increased sales, customer loyalty, and business intelligence reporting. We distribute all of the commission that we collect from merchants to the user in the form of a rebate. This means that the consumer can enjoy more savings through cashback, and the merchant ensures they get more quality customers.'
 	},
-	{
-		question: 'How does it work?',
-		answer: 'Works pretty well'
-	},
-	{
-		question: "Who's the best cofounder?",
-		answer: 'Obviously Sam Collier'
-	}
 ]
--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,14 +1,23 @@
 import Accordion from "../components/accordion/accordion"
 import Footer from "../components/footer"
 import Navbar from "../components/navbar"
+import styled from 'styled-components'
+
+const Flex = styled.div`
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+`
 
 const FaqPage = () => {
 
 	return(
 		<>
-			<Navbar/>
-			<Accordion/>
-			<Footer/>
+			<Flex>
+				<Navbar/>
+				<Accordion/>
+				<Footer/>
+			</Flex>
 		</>
 	)
 }
